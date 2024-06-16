c583683a18fd613762e11f503a9368bed3d2c683
FIRAT MUĞDATOĞLU <frt_4733@hotmail.com>
2024-06-17 00:42:46 +0300

    DartGame
    This code simulates a game of darts by asking the user for the number of throws and generating random coordinates for each dart. It counts how many darts land in zones A, B, C or D and ends if one dart misses all zones. Finally it displays the number and percentage of darts in each zone.

---
+++ DartGame/ConsoleApp1/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("\t\t\t\t\t\t\t\t\tDART GAME");
+            Console.WriteLine();
+
+
+            Console.Write("Enter the number of darts to be thrown:");
+            int numberOfDarts = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            Random random = new Random();
+
+            int counterA = 0;
+            int counterB = 0;
+            int counterC = 0;
+            int counterD = 0;
+
+            int i;
+
+
+
+
+
+            for (i = 1; i <= numberOfDarts; i++)
+            {
+                // -7 ile 7 arasında rasgele bir tamsayı üretin
+                int randomNumberX = random.Next(-7, 8);
+                int randomNumberY = random.Next(-7, 8);
+                
+
+
+                Console.WriteLine("Dart " + i + ":");
+                Console.WriteLine("Coordinates: " + "(" + randomNumberX + "," + randomNumberY + ")");
+                
+                if (randomNumberX >= 0 && randomNumberX <= 5 && randomNumberY >= 0 && randomNumberY <= 5)
+                {
+                    Console.WriteLine("Region: A ");
+                     counterA++;
+                }
+                else if (randomNumberX < 0 && randomNumberX >= -5 && randomNumberY >= 0 && randomNumberY <= 5)
+                {
+                    Console.WriteLine("Region: B ");
+                    counterB++;
+                }
+                else if (randomNumberX < 0 && randomNumberX >= -5 && randomNumberY < 0 && randomNumberY >= -5)
+                {
+                    Console.WriteLine("Region: C ");
+                    counterC++;
+                }
+                else if (randomNumberX >= 0 && randomNumberX <= 5 && randomNumberY < 0 && randomNumberY >= -5)
+                {
+                    Console.WriteLine("Region: D ");
+                    counterD++;
+                }
+                else
+                {
+                    Console.WriteLine("Region: Missed ");
+                    i++;
+                    break;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine();
+
+
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Region statistics:");
+            Console.WriteLine("Total Game: " + (i - 1));
+            Console.WriteLine("A: " + counterA + " dart(s) " + "(" + ((100.0 * counterA / (i - 1)).ToString("F2")) + "%)");
+            Console.WriteLine("B: " + counterB + " dart(s) " + "(" + ((100.0 * counterB / (i - 1)).ToString("F2")) + "%)");
+            Console.WriteLine("C: " + counterC + " dart(s) " + "(" + ((100.0 * counterC / (i - 1)).ToString("F2")) + "%)");
+            Console.WriteLine("D: " + counterD + " dart(s) " + "(" + ((100.0 * counterD / (i - 1)).ToString("F2")) + "%)");
+
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
